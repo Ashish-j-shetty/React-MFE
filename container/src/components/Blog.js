58a132b945ded78736b4e52313c83e22ecdc2403
Ashish J shetty <ashishshetty2612@gmail.com>
2022-06-15 09:37:08 +0530

    MFE container

---
+++ container/src/components/Blog.js
@@ -0,0 +1,13 @@
+import BlogMFE from "./Blog/BlogMFE";
+import Header from "./Header/Header";
+
+const Blog = () => {
+  return (
+    <div>
+      <Header />
+      <BlogMFE />
+    </div>
+  );
+};
+
+export default Blog;
